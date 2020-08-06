670244e3c5ac01e2f725be4806ee864497ce1e94
DmytroBatyuk <dimabatyuk@gmail.com>
2020-08-06 22:23:47 +0300

    added detekt to the project

--- app/src/main/java/com/batyuk/dmytro/kotlinstaticanalysis/MainActivity.kt
+++ app/src/main/java/com/batyuk/dmytro/kotlinstaticanalysis/MainActivity.kt
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //VariableNaming
+    //MagicNumber
     val cUSTOM_VALUE = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FunctionTest()
+
     }
 
+    //FunctionNaming
+    //EmptyFunctionBlock
     private fun FunctionTest() {
 
     }
-}
+}//NewLineAtEndOfFile
